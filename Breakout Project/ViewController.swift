5fe293d7664885175c525534098146b4dc3b39af
student5 <student8@jh-mba158-21.d214.org>
2017-05-02 14:29:04 -0500

    done

--- Breakout Project/ViewController.swift
+++ Breakout Project/ViewController.swift
@@ -8,14 +8,113 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    //@IBOutlet weak var ballView: UIView!      PROGRAM THESE
+    
+    //@IBOutlet weak var paddleOneView: UIView!
+    
+    var dynamicAnimator: UIDynamicAnimator!
+    
+    var pushBehavior: UIPushBehavior!
+    
+    var collisionBehavior: UICollisionBehavior!
+    
+    var ballDynamicBehavior: UIDynamicItemBehavior!
+    
+    var paddleDynamicBehavior: UIDynamicItemBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpViews()
     }
-
-
-
+    
+    
+    func setUpViews()
+    {
+        let paddle = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        paddle.backgroundColor = UIColor.blue
+        view.addSubview(paddle)
+    
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        ballView.layer.cornerRadius = 12.0
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        pushBehavior = UIPushBehavior(items: [ballView], mode: UIPushBehaviorMode.instantaneous)
+        
+        pushBehavior.pushDirection = CGVector(dx: 0.5, dy: 1.0)
+        
+        pushBehavior.active = true
+        
+        
+        pushBehavior.magnitude = 0.3
+        
+        dynamicAnimator.addBehavior(pushBehavior)
+        
+        collisionBehavior = UICollisionBehavior(items: [ballView])
+        
+        collisionBehavior.collisionMode = UICollisionBehaviorMode.everything
+        
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        collisionBehavior.collisionDelegate = self
+        
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        ballDynamicBehavior = UIDynamicItemBehavior(items: [ballView])
+        
+        ballDynamicBehavior.allowsRotation = false
+        
+        ballDynamicBehavior.elasticity = 1.0
+        
+        ballDynamicBehavior.friction = 0.0
+        
+        ballDynamicBehavior.resistance = 0.0
+        
+        dynamicAnimator.addBehavior(ballDynamicBehavior)
+        
+        paddleDynamicBehavior = UIDynamicItemBehavior(items: [paddleOneView])
+        
+        paddleDynamicBehavior.allowsRotation = false
+        
+        paddleDynamicBehavior.density = 1000.0
+        
+        dynamicAnimator.addBehavior(paddleDynamicBehavior)
+        
+        
+    }
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        
+        
+        print("collided")
+        
+    }
+    
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item1: UIDynamicItem, with item2: UIDynamicItem, at p: CGPoint) {
+        self.ballView.backgroundColor = UIColor.cyan
+        print("In second delegate method")
+    }
+    
+    @IBAction func paddleDragged(_ sender: UIPanGestureRecognizer) {
+        
+        paddleOneView.center = CGPoint(x: sender.location(in: self.view).x, y: paddleOneView.center.y)
+        
+        dynamicAnimator.updateItem(usingCurrentState: paddleOneView)
+        
+    }
+    
+    
 }
 

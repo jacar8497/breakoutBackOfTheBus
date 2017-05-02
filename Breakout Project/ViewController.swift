b67c224a8ef7e0cbf130117eb11bf678835b1a54
student5 <student8@jh-mba158-01.d214.org>
2017-05-02 14:39:07 -0500

    paddle and ball

--- Breakout Project/ViewController.swift
+++ Breakout Project/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var paddle = UIView()
+    
+    var ball = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let paddleView = CGRect(x: 167, y: 700, width: 100, height: 25)
+        
+        paddle = UIView(frame: paddleView)
+        
+        let ballView = CGRect(x: 100, y: 100, width: 25, height: 25)
+        
+        ball = UIView(frame: ballView)
+        
+        ball.layer.cornerRadius = 20
+        
+        paddle.backgroundColor = UIColor.black
+        
+        ball.backgroundColor = UIColor.black
+        
+        view.addSubview(paddle)
+        
+        view.addSubview(ball)
+        
+
     }
 
 
